e26f40506915fbe483fb40b04376cf1d33db4e64
Bekir Kaplan <bekirkaplannnn@gmail.com>
2024-05-04 21:54:58 +0300

    finished

--- src/components/legends/LegendContainer.jsx
+++ src/components/legends/LegendContainer.jsx
@@ -3,22 +3,25 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import LegendCard from './LegendCard';
 import  {data} from "../../data"
+import { useState } from 'react';
 
 function SizesExample() {
+const [search,setSearch]= useState("")
+const handleChange = (e) =>{
+  setSearch(e.target.value)
+}
+ const filteredData = data.filter(item => item.name.toLocaleLowerCase().includes(search.trim().toLocaleLowerCase()))
 
-
-
-  console.log(data);
   return (
     <Container className=' m-5 d-flex align-items-center justify-content-center flex-column  '>
-      <InputGroup className="mb-3 w-75 " type="search">
+      <InputGroup className="mb-3 w-75 " type="search" onChange={handleChange}>
         <Form.Control type='search'
           aria-label="Default"
           aria-describedby="inputGroup-sizing-default"
         />
       </InputGroup>
       <Row className=' justify-content-center gap-2'>
-        {data.map(player=>(
+        {filteredData.map(player=>(
 
           <LegendCard key={player.id} player={player}/>
 
